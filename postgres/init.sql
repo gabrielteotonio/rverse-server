48483dcb412d72574068006e245e41492193d609
gabrielteotonio <gabriel.teotonio@ubee.in>
2020-11-17 23:26:57 -0300

    feat(postgres): add folder for custom build of postgres container

---
+++ postgres/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.nz_migration(
+  year_month VARCHAR ( 50 ),	
+  month_of_release VARCHAR ( 50 ),	
+  passenger_type VARCHAR ( 50 ),
+  direction VARCHAR ( 50 ),	
+  citizenship VARCHAR ( 50 ),	
+  visa VARCHAR ( 50 ),	
+  country_of_residence VARCHAR ( 250 )
+);
+
+
+COPY public.nz_migration(year_month, month_of_release, passenger_type, direction, citizenship, visa, country_of_residence) 
+FROM '/var/lib/postgresql/data/international-migration-nz.csv' WITH (FORMAT CSV, HEADER FALSE);
